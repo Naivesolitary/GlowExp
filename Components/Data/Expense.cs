cfd7ff13c26855989e98f6e20c3bcb67fb99854d
Kaixuer <paradoxm75@gmail.com>
2025-01-15 09:13:33 +0545

    Add Successful Tailwind CSS Integration

--- Components/Data/Expense.cs
+++ Components/Data/Expense.cs
@@ -4,5 +4,13 @@ namespace GlowExp.Components.Data
 {
     public class Expense
     {
-    }
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public string? Category { get; set; }
+        public string? Account { get; set; }
+        public string? Note { get; set; }
+
+    
+}
 }
