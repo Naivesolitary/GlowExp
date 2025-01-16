6c2ec39da4b7b84b02e86e237e0596b63b51d621
Kaixuer <paradoxm75@gmail.com>
2025-01-16 15:55:45 +0545

    minimal-income-expense-debt-FE

--- Components/Data/Debt.cs
+++ Components/Data/Debt.cs
@@ -3,5 +3,13 @@ namespace GlowExp.Components.Data
 {
     public class Debt
     {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Amount { get; set; }
+        public string? Tag {get; set;}
+        public string? Source { get; set; }
+        public DateTime DueDate{ get; set; }
+        public string? Note { get; set; }
+
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./wwwroot/index.html","./Pages/**/*.razor"],
+    theme: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'],
+        },
+      extend: {
+        
+      },
+  },
+  plugins: [],
+}
+
